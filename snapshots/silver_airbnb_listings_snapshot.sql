445382eedfc7cb85ae534d3e2c7ef8b2782f0fdf
Hemang Sharma <Hemang.Sharma@student.uts.edu.au>
2024-10-24 21:33:00 +0000

    new snapshot

---
+++ snapshots/silver_airbnb_listings_snapshot.sql
@@ -0,0 +1,35 @@
+{% snapshot silver_airbnb_listings_snapshot %}
+
+{{ config(
+    target_schema='dbt_assignment_silver',
+    unique_key='listing_id',
+    strategy='timestamp',
+    updated_at='scraped_date'
+) }}
+
+SELECT 
+    "LISTING_ID",
+    "SCRAPE_ID",
+    "SCRAPED_DATE",
+    "HOST_ID",
+    HOST_NAME,
+    "HOST_SINCE",
+    "HOST_IS_SUPERHOST",
+    HOST_NEIGHBOURHOOD,
+    LISTING_NEIGHBOURHOOD,
+    PROPERTY_TYPE,
+    ROOM_TYPE,
+    "ACCOMMODATES",
+    PRICE,
+    HAS_AVAILABILITY,
+    "AVAILABILITY_30",
+    "NUMBER_OF_REVIEWS",
+    REVIEW_SCORES_RATING,
+    REVIEW_SCORES_ACCURACY,
+    REVIEW_SCORES_CLEANLINESS,
+    REVIEW_SCORES_CHECKIN,
+    REVIEW_SCORES_COMMUNICATION,
+    REVIEW_SCORES_VALUE
+FROM {{ ref('silver_airbnb_listings') }}
+
+{% endsnapshot %}

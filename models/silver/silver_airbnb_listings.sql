701567bffaf004ed2eae1415caaf3489cef173d2
Hemang Sharma <Hemang.Sharma@student.uts.edu.au>
2024-10-24 10:29:20 +0000

    5th

--- models/silver/silver_airbnb_listings.sql
+++ models/silver/silver_airbnb_listings.sql
@@ -1,26 +1,8 @@
--- models/silver/silver_airbnb_listings.sql
 WITH listings_clean AS (
     SELECT 
-        listing_id,
-        host_id,
-        host_is_superhost,
-        host_neighbourhood,
-        listing_neighbourhood,
-        property_type,
-        room_type,
-        accommodates,
-        price::NUMERIC AS price,
-        has_availability,
-        availability_30::INT,
-        number_of_reviews::INT,
-        review_scores_rating::NUMERIC,
-        review_scores_accuracy::NUMERIC,
-        review_scores_cleanliness::NUMERIC,
-        review_scores_checkin::NUMERIC,
-        review_scores_communication::NUMERIC,
-        review_scores_value::NUMERIC
+        "listing_id",
+        "host_id"
     FROM {{ ref('bronze_airbnb_listings') }}
-    WHERE has_availability = 't'
 )
 SELECT *
 FROM listings_clean

3e4105e299b13942fda464e7866b545406f7b294
Hemang Sharma <Hemang.Sharma@student.uts.edu.au>
2024-10-24 21:11:53 +0000

    Gold model done

--- models/gold/datamart/dm_listing_neighbourhood.sql
+++ models/gold/datamart/dm_listing_neighbourhood.sql
@@ -2,15 +2,15 @@
 WITH neighbourhood_metrics AS (
     SELECT
         listing_neighbourhood,
-        COUNT(listing_id) AS active_listings,
-        MIN(price) AS min_price,
-        MAX(price) AS max_price,
-        AVG(price) AS avg_price,
-        COUNT(DISTINCT host_id) AS distinct_hosts,
+        COUNT("LISTING_ID") AS active_listings,
+        MIN(PRICE) AS min_price,
+        MAX(PRICE) AS max_price,
+        AVG(PRICE) AS avg_price,
+        COUNT(DISTINCT "HOST_ID") AS distinct_hosts,
         AVG(review_scores_rating) AS avg_review_score,
-        SUM(30 - availability_30) AS total_stays,
-        SUM(price * (30 - availability_30)) AS estimated_revenue,
-        (COUNT(DISTINCT host_id) FILTER(WHERE host_is_superhost = 't') * 100.0 / COUNT(DISTINCT host_id)) AS superhost_rate
+        SUM(30 - "AVAILABILITY_30") AS total_stays,
+        SUM(price * (30 - "AVAILABILITY_30")) AS estimated_revenue,
+        (COUNT(DISTINCT "HOST_ID") FILTER(WHERE "HOST_IS_SUPERHOST" = 't') * 100.0 / COUNT(DISTINCT "HOST_ID")) AS superhost_rate
     FROM {{ ref('silver_airbnb_listings') }}
     WHERE has_availability = 't'
     GROUP BY listing_neighbourhood
--- models/gold/datamart/dm_property_type.sql
+++ models/gold/datamart/dm_property_type.sql
@@ -1,20 +1,20 @@
 -- models/datamart/dm_property_type.sql
 WITH property_metrics AS (
     SELECT
-        property_type,
-        room_type,
-        accommodates,
-        COUNT(listing_id) AS active_listings,
-        MIN(price) AS min_price,
-        MAX(price) AS max_price,
-        AVG(price) AS avg_price,
-        COUNT(DISTINCT host_id) AS distinct_hosts,
+        PROPERTY_TYPE,
+        ROOM_TYPE,
+        "ACCOMMODATES",
+        COUNT("LISTING_ID") AS active_listings,
+        MIN(PRICE) AS min_price,
+        MAX(PRICE) AS max_price,
+        AVG(PRICE) AS avg_price,
+        COUNT(DISTINCT "HOST_ID") AS distinct_hosts,
         AVG(review_scores_rating) AS avg_review_score,
-        SUM(30 - availability_30) AS total_stays,
-        SUM(price * (30 - availability_30)) AS estimated_revenue
+        SUM(30 - "AVAILABILITY_30") AS total_stays,
+        SUM(PRICE * (30 - "AVAILABILITY_30")) AS estimated_revenue
     FROM {{ ref('silver_airbnb_listings') }}
     WHERE has_availability = 't'
-    GROUP BY property_type, room_type, accommodates
+    GROUP BY PROPERTY_TYPE, ROOM_TYPE, "ACCOMMODATES"
 )
 SELECT *
 FROM property_metrics

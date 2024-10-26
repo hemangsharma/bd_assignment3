1375911660abf7d7246e231e6ab814f8cc3adedc
Hemang Sharma <Hemang.Sharma@student.uts.edu.au>
2024-10-26 01:38:03 +0000

    change

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,7 +1,7 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
     {%- if custom_schema_name is none -%}
-        {{ target.schema }}  # Use target schema if custom schema name is not provided
+        {{ target.schema }}
     {%- else -%}
-        {{ custom_schema_name | trim }}  # Use custom schema name directly
+        {{ custom_schema_name | trim }} 
     {%- endif -%}
 {%- endmacro %}

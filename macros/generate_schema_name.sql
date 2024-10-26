660e789f71a0ee5922b3ec5265f1f1bfea12e58e
Hemang Sharma <Hemang.Sharma@student.uts.edu.au>
2024-10-26 01:07:56 +0000

    macros

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,14 +1,7 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
-    {%- set default_schema = target.schema -%}
-
-    {%- if target.name == 'prod' -%}
-        {%- if custom_schema_name is none -%}
-            {{ default_schema }}
-        {%- else -%}
-            {{ custom_schema_name | trim }}
-        {%- endif -%}
+    {%- if custom_schema_name is none -%}
+        {{ target.schema }}  # Use target schema if custom schema name is not provided
     {%- else -%}
-        {{ default_schema }}_{{ custom_schema_name | trim }}
+        {{ custom_schema_name | trim }}  # Use custom schema name directly
     {%- endif -%}
-
 {%- endmacro %}

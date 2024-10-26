04268b5fcfa2237bc1e3eed8ce02c3b2db8d6ef7
Hemang Sharma <Hemang.Sharma@student.uts.edu.au>
2024-10-26 01:50:32 +0000

    aaa

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,7 +1,13 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
-    {%- if custom_schema_name is none -%}
-        {{ target.schema }}
+    {%- set default_schema = target.schema -%}
+
+    {%- if target.name == 'prod' -%}
+        {%- if custom_schema_name is none -%}
+            {{ default_schema }}
+        {%- else -%}
+            {{ custom_schema_name | trim }}
+        {%- endif -%}
     {%- else -%}
-        {{ custom_schema_name | trim }} 
+        {{ default_schema }}_{{ custom_schema_name | trim }}
     {%- endif -%}
 {%- endmacro %}
--- macros/get_limit_subquery_sql.sql
+++
@@ -1,5 +0,0 @@
---macros/get_limit_subquery_sql.sql
-
-{% macro default__get_limit_subquery_sql(sql, limit) %}
-    {{ sql }}
-{% endmacro %}
